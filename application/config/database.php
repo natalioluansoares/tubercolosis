a793d253b5eb122db8e099687a66717681a3a343
natalioluansoares <natalioluansoares@gmail.com>
2022-06-09 15:41:38 +0800

    Tubercolosis Skripsi

--- application/config/database.php
+++ application/config/database.php
@@ -1,5 +1,5 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 /*
 | -------------------------------------------------------------------
@@ -78,7 +78,7 @@ $db['default'] = array(
 	'hostname' => 'localhost',
 	'username' => 'root',
 	'password' => '',
-	'database' => 'informasi_rumahsakit',
+	'database' => 'tubercolosis',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
